bbfb569fcc243f6e5cdc2821cf769aa92d40183c
Tara Sabella <tarasabella@gmail.com>
2018-12-14 17:28:19 +0000

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,22 @@
 require_relative "./vehicle.rb"
+#subclass for differnt types of 
+# the parent/super class (Vehicle) 
+
+class Car <
+  Vehicle 
+  
+def go 
+  "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+end
+
+
+# < used to inherit the car class#from Vehicle (super/parent class)
+
+#Car class inherits all of the vehicle methods so we need to access them 
+#throw in the go method and its differnt than the return of the go method inherited from teh Vehicle class 
+#so we overwite the inherited go with one speciffic to car 
+# when go method is invoked, the comiler 
+#looks in the class to which the instance of car that we call the method on belonghs 
+# if go method is there it will exectue the method 
+# if it cant find the go methiodl it will take the one in the parent class that this class inherites from 
--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,22 @@
 class Vehicle
+  #parent/super class 
+  #the other classes can adopt all of the#
+  #vehicles attributes and behavior (method)
 
-end
+  attr_accessor :wheel_size, :wheel_number
+ 
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end
+ 
+  def go
+    "vrrrrrrrooom!"
+  end
+ 
+  def fill_up_tank
+    "filling up!"
+  end
+ 
+
+end 
